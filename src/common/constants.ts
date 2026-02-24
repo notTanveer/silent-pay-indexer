dd68375816f2dad692bfca0c533fdf45bcae5fea
notTanveer <tanveer.x.ansari@gmail.com>
2026-02-24 15:55:22 +0530

    update max block range to 100

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -11,4 +11,4 @@ export const BITCOIN_CORE_FULL_VERBOSITY_VERSION = 23_0000;
 
 export const SILENT_PAYMENT_BLOCK_TYPE = 0x00;
 
-export const MAX_BLOCK_RANGE = 50;
+export const MAX_BLOCK_RANGE = 100;
